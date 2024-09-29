a373b5a16b602f87aed788d2b36c536aa374047c
Meeran Ali <azlanirfan98@gmail.com>
2024-09-29 16:57:36 +0500

    Add files via upload

---
+++ Class 33/main.js
@@ -0,0 +1,51 @@
+
+
+
+
+// var name1 = prompt("enter your name");
+
+// var updatedName = ''
+
+// for(var i =name1.length -1; i >= 0; i--){
+//     updatedName+= name1[i];
+
+// }
+// alert(name1 + "\n" + updatedName);
+// if (name1===updatedName) {
+//     alert("true")
+
+
+// } 
+//  else{
+//     alert("false")
+//  }
+
+
+
+// var num  = Number (prompt("enter your number"));
+// var num2 =  Math.round(Math.random(num)*10)
+
+// alert(num2);
+
+
+// var arr = ["a", "b", 1, 2, true, 3, "c", "d", false, "false", 4, 5];
+
+// alert("Values of  Array :-" + " " + "(" + arr + ")")
+
+// var strings = [];
+// var numbers = [];
+// var booleans = [];
+
+
+// for (var i = 0; i < arr.length; i++) {
+//     var element = arr[i];
+//     if (typeof element === 'string') {
+//         strings.push(element);
+//     } else if (typeof element === 'number') {
+//         numbers.push(element);
+//     } else if (typeof element === 'boolean') {
+//         booleans.push(element);
+//     }
+// }
+
+// console.log("1" + "\n" +"Strings:" + "("+strings+ ")" + "\n" + "2" + "/n" +"Numbers:" + "("+numbers + ")"+ "\n" + "Booleans:", booleans);
